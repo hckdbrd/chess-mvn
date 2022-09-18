4b1fbd93bb82f8956f53be85b265daaf66ad204b
hcdkbrd <ennbosh@icloud.com>
2022-09-18 23:41:35 +0300

    Add: PWA settings and app.properties .

--- src/main/java/com/project/chess/Application.java
+++ src/main/java/com/project/chess/Application.java
@@ -1,13 +1,21 @@
 package com.project.chess;
 
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.server.PWA;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+@PWA(
+        name = "Gambit",
+        shortName = "Gambit",
+        offlinePath="./offline.html",
+        offlineResources = { "./images/offline.png"}
+)
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
-
 }
